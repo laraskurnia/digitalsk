9b7440176cfaf94548b73e6e22482ff01f07f0cf
Rushel <ruselaq@gmail.com>
2025-01-29 15:11:54 +0800

    fix database scheme

--- app/Models/Mpangkat.php
+++ app/Models/Mpangkat.php
@@ -10,7 +10,7 @@ class MPangkat extends Model
     use HasFactory;
 
     // Tentukan nama tabel yang digunakan
-    protected $table = 'pangkat';  // Ganti dengan nama tabel yang benar
+    protected $table = 'pangkats';  // Ganti dengan nama tabel yang benar
 
     // Tentukan kolom yang bisa diisi secara massal
     protected $fillable = [
